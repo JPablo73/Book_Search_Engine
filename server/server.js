f6e6588b4d38f9af0b7b1775bd3adca3d183758a
JPablo73 <82916926+JPablo73@users.noreply.github.com>
2021-11-04 17:20:01 -0700

    bug fix, server

--- server/server.js
+++ server/server.js
@@ -1,20 +1,23 @@
 const express = require("express");
+// import ApolloServer
 const { ApolloServer } = require("apollo-server-express");
+// import our typeDefs and resolvers
 const { typeDefs, resolvers } = require("./schemas");
 const path = require("path");
 const db = require("./config/connection");
 
+// import middleware
 const { authMiddleware } = require("./utils/auth");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
-
+// create a new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware,
 });
-
+// integrate our Apollo server with the Express application as middleware
 server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: false }));
@@ -35,6 +38,6 @@ db.once("open", () => {
   console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 });
 
-// app.get("*", (req, res) => {
-//   res.status(404).sendFile(path.join(__dirname, "./public/404.html"));
-// });
+app.get("*", (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, "./public/404.html"));
+});
